608fbba5bad4fad3d716fad35c019ab6a2a56cd7
Shirley <Shirley@Andres>
2018-09-14 15:06:13 -0600

    Upgrade

--- src/ac/itcr/perez_moya/datos1/library/Library.java
+++ src/ac/itcr/perez_moya/datos1/library/Library.java
@@ -10,8 +10,59 @@ package ac.itcr.perez_moya.datos1.library;
  * @author julatec
  */
 public class Library {
-    private String pais, location;
-    private int id;
+    private String country, location, nameLib;
+    private int id, horario;
+
+    public Library(String country, String location, String nameLib, int id, int horario) {
+        this.country = country;
+        this.location = location;
+        this.nameLib = nameLib;
+        this.id = id;
+        this.horario = horario;
+    }
+
+    public Library() {
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getNameLib() {
+        return nameLib;
+    }
+
+    public void setNameLib(String nameLib) {
+        this.nameLib = nameLib;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHorario() {
+        return horario;
+    }
+
+    public void setHorario(int horario) {
+        this.horario = horario;
+    }
     
     
 }
--- src/ac/itcr/perez_moya/datos1/library/Openhours.java
+++ src/ac/itcr/perez_moya/datos1/library/Openhours.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ac.itcr.perez_moya.datos1.library;
 
-/**
- *
- 
- */
 public class Openhours {
     int start, end;
 
