eaaf18c34b327ca9fb2f2ee7a75e8bd5d596cb4b
John Doe <andresperez400@live.com>
2018-10-26 17:50:42 -0600

    La clase del HealthOffice. Enum del status según el color.

---
+++ src/tareaprogramada2/datos1/register/ColorCondition.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaprogramada2.datos1.register;
+
+public class ColorCondition {
+    public enum ColorStatus{
+        Rojo,
+        Amarillo,
+        Verde;
+    }
+}
