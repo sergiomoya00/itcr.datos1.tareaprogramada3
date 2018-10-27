eda5af6203c27f9d3696b37a5b0025a68752cf52
John Doe <andresperez400@live.com>
2018-10-27 17:10:53 -0600

    Organización de carpetas y clases

---
+++ src/tareaprogramada2/datos1/register/attendedPeople/AttentionCenter.java
@@ -0,0 +1,14 @@
+package tareaprogramada2.datos1.register.attendedPeople;
+import java.util.Random;
+import tareaprogramada2.datos1.register.Patient;
+
+public class AttentionCenter {
+    
+    Patient person =
+           
+    public Patient attentionProcess(){
+        
+        return 
+    }
+    
+}
