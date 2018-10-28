254e958563e87d9741116b09c7079a1a969c9495
John Doe <andresperez400@live.com>
2018-10-28 16:29:31 -0600

    Organización de carpetas y clases

---
+++ test/PriorityQueue/PriorityQueue.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PriorityQueue;
+
+import tareaprogramada2.datos1.register.*;
+import tareaprograma2.datos1.collections.*;
+
+public class PriorityQueue {
+    
+    public static void main(String [] args){
+    
+    Ticket uno = new Ticket();
+    Ticket dos = new Ticket();
+    Ticket tres = new Ticket();
+    Ticket cuatro = new Ticket();
+    Ticket cinco = new Ticket();
+    Ticket seis = new Ticket();
+    
+    
+    
+}
+}
