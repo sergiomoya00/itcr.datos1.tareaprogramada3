726a325bdaa5f31e6d04c8add0867afb1a2aed68
Ronaldo Picado Vega <ronaldopv08@gmail.com>
2018-10-28 17:30:34 -0600

    Merge origin/master
    Conflicts:
    	src/tareaprograma2/datos1/collections/MyHeap.java

---
+++ src/tareaprogramada2/datos1/register/DurationProvider.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaprogramada2.datos1.register;
+
+import java.util.Random;
+
+/**
+ *
+ * 
+ */
+public class DurationProvider {
+    
+    private final int min;
+    private final int max;
+
+    public DurationProvider(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+    
+    public int getDuration(){
+        return min+new Random().nextInt(max-min);
+    }
+    
+}
