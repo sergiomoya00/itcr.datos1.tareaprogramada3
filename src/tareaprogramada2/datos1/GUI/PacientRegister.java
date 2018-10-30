16977c40c51b7e84f6ae2151976d292a53e2df9a
John Doe <andresperez400@live.com>
2018-10-30 00:05:49 -0600

    Merge origin/master
    Conflicts:
    	src/tareaprogramada2/datos1/GUI/PacientRegister.form
    	src/tareaprogramada2/datos1/GUI/PacientRegister.java

--- src/tareaprogramada2/datos1/GUI/PacientRegister.java
+++ src/tareaprogramada2/datos1/GUI/PacientRegister.java
@@ -302,7 +302,7 @@ public class PacientRegister extends javax.swing.JFrame {
 
         jButton9.setText("Aceptar");
 
-        jButton10.setText("jButton10");
+        jButton10.setText("Liberar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
