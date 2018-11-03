06b49c230f36bafc8a80f848639e6134503ed2f9
Shirley <Shirley@Andres>
2018-11-03 00:39:11 -0600

    Creation of an interface for handling the data structures

--- src/main/java/tareaprogramada3/datos1/structures/Graph/Graph.java
+++ src/main/java/tareaprogramada3/datos1/structures/Graph/Graph.java
@@ -5,7 +5,44 @@
  */
 package tareaprogramada3.datos1.structures.Graph;
 
+import tareaprogramada3.datos1.structures.StructuresInterface;
 
-public class Graph {
+
+public class Graph implements StructuresInterface{
+
+    @Override
+    public void add() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void search(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object set(Object object, int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object get(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/main/java/tareaprogramada3/datos1/structures/LinkedList/LinkedList.java
+++ src/main/java/tareaprogramada3/datos1/structures/LinkedList/LinkedList.java
@@ -5,10 +5,47 @@
  */
 package tareaprogramada3.datos1.structures.LinkedList;
 
+import tareaprogramada3.datos1.structures.StructuresInterface;
+
 /**
  *
  * @author 
  */
-public class LinkedList {
+public class LinkedList implements StructuresInterface {
+
+    @Override
+    public void add() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void search(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object set(Object object, int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object get(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
