72f36a37640c51b9deae0fd201032123bb7b9a9c
John Doe <andresperez400@live.com>
2018-11-07 11:12:01 -0600

    Merge origin/master
    Conflicts:
    	.gitignore
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/tareaprogramada3/datos1/structures/Graph/Graph.java
+++ src/tareaprogramada3/datos1/structures/Graph/Graph.java
@@ -9,43 +9,14 @@ import tareaprogramada3.datos1.structuresInterface.StructuresInterface;
 public class Graph<T> implements GraphBehavior<Comparable<T>>{
     
     
-    public LinkedList <VertexNode> vertexes;  //List of vertex of the graph
+    public LinkedList<VertexNode> vertexes;  //List of vertex of the graph
+    public LinkedList<VertexNode> edges;
 
-    @Override
-    public void add(Object value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-  
     
     
-
-    @Override
-    public Object remove(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Object search(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Object set(Object object, int index) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Object get(int index) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
--- src/tareaprogramada3/datos1/structures/Graph/VertexNode.java
+++ src/tareaprogramada3/datos1/structures/Graph/VertexNode.java
@@ -2,28 +2,8 @@
 package tareaprogramada3.datos1.structures.Graph;
 import java.util.*;
 
-public class VertexNode {
-    
-    public int vertex;            //Keeps the data of vertex
-    public boolean visited;       //Indicates if the vertex is being visisted 
-    public LinkedList <GraphNode> edge;  //Keeps the edge list of vertex
-
-    public VertexNode(){
-        this.vertex = 0;
-        this.visited = false;
-        this.edge  = new LinkedList<GraphNode>();
-    } 
-    
-    public VertexNode(int v){
-            this.vertex = v;
-            this.visited = false;
-            this.edge  = new LinkedList<GraphNode>();
-    }
-
-    public String toStringVertexNode() {
-        return "VertexNode{" + "vertex=" + vertex + ", visited=" + visited + ", edge=" + edge + '}';
-    }
-    
-    
-
+public class VertexNode<T> {
+    public String ID;
+    public T element;
+    public boolean visited;     
 }
