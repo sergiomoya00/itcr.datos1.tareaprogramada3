36ede2d5860b229bda11b8c7d423187ec13c930f
John Doe <andresperez400@live.com>
2018-11-10 21:07:17 -0600

    Merge origin/master
    Conflicts:
    	.gitignore
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/tareaprogramada3/datos1/structures/Tree/Tree.java
+++ src/tareaprogramada3/datos1/structures/Tree/Tree.java
@@ -5,9 +5,9 @@
  */
 package tareaprogramada3.datos1.structures.Tree;
 
-import tareaprogramada3.datos1.structuresInterface.StructuresInterface;
+import tareaprogramada3.datos1.structuresInterface.*;
 
-public class Tree<T extends Comparable<T>> implements StructuresInterface {
+public class Tree<T extends Comparable<T>> implements TreeBehavior<T> {
 
     TreeNode<T> root;
     public int size;
@@ -17,7 +17,7 @@ public class Tree<T extends Comparable<T>> implements StructuresInterface {
         size = 0;
     }
 
-    
+     @Override
     public void add(T element) {
         TreeNode newNode = new TreeNode(element);
         if (isEmpty()) {
@@ -44,75 +44,10 @@ public class Tree<T extends Comparable<T>> implements StructuresInterface {
                 }
             }
         }
-
-    }
-    
-    public boolean isLeaf(TreeNode Node){
-        if((Node.subLeft != null) & (Node.subRight!= null)){
-            return true;
-        }
-        return false;
-   
-    }
-    
-    public T searchMin(){
-        TreeNode<T> current = this.root;
-        
-        while (! current.subLeft.isEmpty()){     //No lo lee, supongo que por estar en la interfaz
-            current = current.subLeft;
-        }
-        
-        return current.getValue();
     }
     
     
-    public T searchMax(){
-        TreeNode<T> current = this.root;
-        
-        while (! current.subRight.isEmpty()){     //No lo lee, supongo que por estar en la interfaz
-            current = current.subRight;
-        }
-        return current.getValue();
-    }
     
-    @Override
-    public Object remove(Object element) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public Object search(T element) {
-        if (isEmpty()) {
-            return null;
-        }
-
-        TreeNode<T> current = root;
-        TreeNode<T> father;
-
-        while (true) {
-            father = current;
-
-            if (current == null) {
-                break;
-            }
-
-            if (current.getValue().equals(element)) {
-                break;
-            } 
-            else if ((current.getValue()).compareTo(element) < 0) { //NO SE EJECUTA BIEN EL CompareTo
-                current = current.subLeft;
-            }
-            else {
-                current = current.subRight;
-            }
-        }
-
-        if (current == null) {
-            return null;
-        }
-
-        return current.getValue();
-    }
 
     @Override
     public int size() {
@@ -135,13 +70,45 @@ public class Tree<T extends Comparable<T>> implements StructuresInterface {
     }
 
     @Override
-    public void add(Object element) {
+    public Comparable remove(Comparable element) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Object search(Object element) {
+    public Comparable search(Comparable element) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void preOrderTrail(TreeNode current) {  //Se implementa igual a root SUPONGO
+        if (current != null){
+            preOrderTrail(current.subLeft);
+            preOrderTrail(current.subRight);
+        }
+    }
+
+    @Override
+    public void postOrderTrail(TreeNode current) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+    @Override
+    public void orderTrail(TreeNode current) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void widthTrail(TreeNode current) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isLeaf(TreeNode node) {       
+        if((node.subLeft != null) & (node.subRight!= null)){
+            return true;
+        }
+        return false;
     }
 
 }
