b8ff095c5aefc8afa3d9990ec563c085fa41ae1b
Ronaldo Picado Vega <ronaldopv08@gmail.com>
2018-11-21 15:48:52 -0600

    Merge origin/master
    Conflicts:
    	pom.xml

---
+++ src/main/java/tareaprogramada3/datos1/structures/Graph/Graph.java
@@ -0,0 +1,47 @@
+
+package tareaprogramada3.datos1.structures.Graph;
+
+import java.util.*;
+import tareaprogramada3.datos1.structuresInterface.GraphBehavior;
+
+import tareaprogramada3.datos1.structuresInterface.StructuresInterface;
+
+public class Graph<T> implements GraphBehavior<Comparable<T>>{
+    
+    
+    public LinkedList<VertexNode> vertexes;  //List of vertex of the graph
+    public LinkedList<VertexNode> edges;
+
+    
+    
+    
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void add(Comparable<T> element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Comparable<T> remove(Comparable<T> element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Comparable<T> search(Comparable<T> element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+
+   
+    
+}
