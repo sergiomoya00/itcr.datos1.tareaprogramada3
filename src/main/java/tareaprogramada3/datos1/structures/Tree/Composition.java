21c07e0f2d6951f3b267309fdc00bb1c1a77b0ac
John Doe <andresperez400@live.com>
2018-11-23 00:20:47 -0600

    Merge origin/master
    Conflicts:
    	src/main/java/tareaprogramada3/datos1/administration/Clients.java

--- src/main/java/tareaprogramada3/datos1/structures/Tree/Composition.java
+++ src/main/java/tareaprogramada3/datos1/structures/Tree/Composition.java
@@ -3,6 +3,7 @@ package tareaprogramada3.datos1.structures.Tree;
 import java.util.Comparator;
 
 class Composition implements Comparator<Integer>
+//MÉTODO PARA EL COMPARATOR
 {
    public int compare(Integer x, Integer y)
    {
