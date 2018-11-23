3bae92fd0a5ce9fdd6f81e2ecf802ba97e1643e0
John Doe <andresperez400@live.com>
2018-11-22 22:12:27 -0600

    Merge origin/master
    Conflicts:
    	pom.xml

--- src/main/java/tareaprogramada3/datos1/administration/Clients.java
+++ src/main/java/tareaprogramada3/datos1/administration/Clients.java
@@ -5,10 +5,65 @@
  */
 package tareaprogramada3.datos1.administration;
 
+import graph.Grafo;
+
 /**
  *
  * @author
  */
 public class Clients {
+    private String name;
+    private int id;
+    private int telephone;
+    private String mail;
+    private String birth;
+    private Grafo subgraph;
+
+    public Grafo getSubgraph() {
+        return subgraph;
+    }
+
+    public void setSubgraph(Grafo subgraph) {
+        this.subgraph = subgraph;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
     
 }
