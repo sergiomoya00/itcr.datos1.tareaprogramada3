ddee9d2802c83b7f6e8ec3885a1e2d9dc8eec82f
Ronaldo Picado Vega <ronaldopv08@gmail.com>
2018-11-23 23:50:55 -0600

    Merge origin/master
    Conflicts:
    	pom.xml

---
+++ src/main/java/tareaprogramada3/datos1/structures/Graph/Grafo.java
@@ -0,0 +1,53 @@
+package tareaprogramada3.datos1.structures.Graph;
+
+import java.util.List;
+
+/**
+ * La clase grafo permite generar los linkeos entre los v�rtices y las aristas
+
+ */
+
+//UNE VERTICES Y ARISTAS
+public class Grafo {
+	private List<Vertice<Object>> vertices;
+	private List<Arista> aristas;
+
+	/**
+	 * constructor del grafo
+	 * 
+	 * @param pVertices lista de vertices del grafo
+	 * @param pAristas  lista de aristas del grafo
+	 */
+	public Grafo(List<Vertice<Object>> pVertices, List<Arista> pAristas) {
+		this.vertices = pVertices;
+		this.aristas = pAristas;
+	}
+	// GETTERS Y SETTERS
+
+	public List<Vertice<Object>> getVertices() {
+		return vertices;
+	}
+
+	public void setVertices(List<Vertice<Object>> vertices) {
+		this.vertices = vertices;
+	}
+
+	public List<Arista> getAristas() {
+		return aristas;
+	}
+
+	public void setAristas(List<Arista> aristas) {
+		this.aristas = aristas;
+	}
+
+	@Override
+	/**
+	 * método para obtener las variables en string
+	 */
+	public String toString() {
+		String msj = "";
+		msj += vertices.toString();
+		msj += aristas.toString();
+		return msj;
+	}
+}
